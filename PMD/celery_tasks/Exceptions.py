377e92ee64c3c8a619e29a0c9cd6040934b83e75
Benjamin Mampaey <benjamin.mampaey@oma.be>
2014-07-23 11:51:30 +0200

    Adding unittest for celery tasks

---
+++ PMD/celery_tasks/Exceptions.py
@@ -0,0 +1,18 @@
+class SetupError(Exception):
+	pass
+
+class FileNotFound(Exception):
+	pass
+
+class ConnectionError(Exception):
+	pass
+
+class DownloadError(Exception):
+	pass
+
+class LocateError(Exception):
+	pass
+
+class UpdateError(Exception):
+	pass
+
