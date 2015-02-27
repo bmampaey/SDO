970c14b16d22c2971435097197d74670d811f261
Benjamin Mampaey <bmampaey@gmail.com>
2015-02-27 15:32:23 +0100

    corrected tasks name in schedule

--- tasks/tasks_schedule.py
+++ tasks/tasks_schedule.py
@@ -5,32 +5,32 @@ from celery.schedules import crontab
 
 celery_beat_schedule = {
 	'execute_data_download_requests': {
-		'task': 'tasks.execute_data_download_requests',
+		'task': 'tasks.data_request_tasks.execute_data_download_requests',
 		'schedule': timedelta(minutes=5),
 		'args': ()
 	},
 	'execute_data_delete_requests': {
-		'task': 'tasks.execute_data_delete_requests',
+		'task': 'tasks.data_request_tasks.execute_data_delete_requests',
 		'schedule': timedelta(minutes=5),
 		'args': ()
 	},
 	'execute_data_location_requests': {
-		'task': 'tasks.execute_data_location_requests',
+		'task': 'tasks.data_request_tasks.execute_data_location_requests',
 		'schedule': timedelta(minutes=5),
 		'args': ()
 	},
 	'execute_metadata_update_requests': {
-		'task': 'tasks.execute_metadata_update_requests',
+		'task': 'tasks.data_request_tasks.execute_metadata_update_requests',
 		'schedule': timedelta(minutes=5),
 		'args': ()
 	},
 	'sanitize_local_data_location': {
-		'task': 'tasks.sanitize_local_data_location',
+		'task': 'tasks.data_management_tasks.sanitize_local_data_location',
 		'schedule': crontab(hour=0, minute=0),
 		'args': ()
 	},
 	'create_SDO_synoptic_tree': {
-		'task': 'tasks.create_SDO_synoptic_tree',
+		'task': 'tasks.data_management_tasks.create_SDO_synoptic_tree',
 		'schedule': crontab(hour=23, minute=0),
 		'args': ("1H_synoptic",)
 	},
